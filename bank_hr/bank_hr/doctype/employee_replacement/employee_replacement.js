713ff106d9d9391a996d4386fc2e05b91c62c02c
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2025-02-09 17:41:01 +0200

    filter for branch

--- bank_hr/bank_hr/doctype/employee_replacement/employee_replacement.js
+++ bank_hr/bank_hr/doctype/employee_replacement/employee_replacement.js
@@ -1,8 +1,29 @@
 // Copyright (c) 2025, mina and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Employee Replacement", {
-// 	refresh(frm) {
+// Copyright (c) 2025, Mina and contributors
+// For license information, please see license.txt
 
-// 	},
-// });
+frappe.ui.form.on("Employee Replacement", {
+  to_branch: function(frm) {
+      let branch = frm.doc.to_branch;
+
+      frappe.db.get_list("Employee", { 
+          filters: { "branch": branch }, 
+          fields: ["name"]
+      }).then(employees => {
+          let employee_names = employees.map(emp => emp.name); // Extract names
+
+          console.log("Employees:", employee_names);
+
+          frm.set_query("replace_with_employee", function() {
+              return {
+                  filters: {
+                      name: ["in", employee_names]
+                  }
+              };
+          });
+      });
+  }
+});
+
